2ea30e9b057ec2ac61f99c48e9c4d4e4357b69d5
zhaofulu <zhao.fulu@chinaott.onaliyun.com>
2018-07-27 13:00:22 +0800

    修改访问量

--- assets/js/myRequest.js
+++ assets/js/myRequest.js
@@ -1,7 +1,7 @@
 (function(){
     $.ajax({
         type: 'POST',
-        url: 'http://192.168.10.232:8761/addr',
+        url: 'http://47.93.198.23:8761/addr',
         data: JSON.stringify({"cip":"000000000", "cid":"1000000", "cname":"cname北京"}),
         success: function (date) {
             document.getElementById("viewCount").innerHTML = "总访问量：<lable style='color: dodgerblue;font-size: 70px'>"+date+"</lable>";
