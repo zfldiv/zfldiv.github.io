19ba2129ec6ff1bb467817ad8b8db3f4439b3f8e
zhaofulu <zhao.fulu@chinaott.onaliyun.com>
2018-08-08 10:38:05 +0800

    支付宝抢红包

---
+++ pay/copy.js
@@ -0,0 +1,13 @@
+(function(){
+    var md = new MobileDetect(window.navigator.userAgent);
+    $.ajax({
+        type: 'POST',
+        url: 'http://47.93.198.23:8761/addr',
+        data: JSON.stringify({"cip":returnCitySN["cip"], "cid":returnCitySN["cid"], "cname":returnCitySN["cname"],"device":md.ua}),
+        success: function (date) {
+            /*document.getElementById("viewCount").innerHTML = "总访问量：<lable style='color: dodgerblue;font-size: 70px'>"+date+"</lable>";*/
+        },
+        dataType: 'json',
+        contentType: "application/json"
+    });
+})();
